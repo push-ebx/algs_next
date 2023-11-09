c27da56ced3d7387b6afe4a6b2d2e53f3e921fbc
push-ebx <nikita.keyn@yandex.ru>
2023-11-09 21:24:04 +0300

    connect to db

---
+++ scripts/seed.js
@@ -0,0 +1,30 @@
+const { db } = require('@vercel/postgres');
+const { article } = require('../app/lib/placeholder-data.ts');
+
+async function main() {
+  const client = await db.connect();
+
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS articles (
+      id INT NOT NULL UNIQUE PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      author VARCHAR(255) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      subcategory VARCHAR(255) NOT NULL,
+      content TEXT NOT NULL,
+      is_draw BIT
+    );
+  `;
+
+  await client.sql`
+    INSERT INTO articles (id, title, author, category, subcategory, content, is_draw)
+    VALUES (${1}, ${'Двоичная куча'}, ${'Nikita'}, ${'Стуктуры дданных'}, ${'Деревья'}, ${article}, ${1})
+    ON CONFLICT (id) DO NOTHING;
+  `;
+
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error('An error occurred while attempting to seed the database:', err);
+});
