97a476bf9a9e37d421ac1cfbd7c8e4230fb323c1
push-ebx <nikita.keyn@yandex.ru>
2023-11-16 18:37:36 +0300

    fix

--- app/ui/markdown/index.tsx
+++ app/ui/markdown/index.tsx
@@ -3,36 +3,15 @@
 import styles from './markdown.module.scss'
 import Markdown from "markdown-to-jsx";
 import { CSSProperties } from "react";
-import {Paragraph, Subtitle, Title, Li, Header} from "@/app/ui";
+import {Paragraph, Subtitle, Title, Li, Test} from "@/app/ui";
 // import Image from "next/image";
-// import ReactFlow from 'reactflow';
-//
-// import 'reactflow/dist/style.css';
 
 type Props = {
   children?: string,
   style?: CSSProperties,
   className?: string
 }
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-const Test = ({label, height}:{label: string, height: number}) => {
-  return (
-    <div style={{width: '100%', height: `${height}px`}}>
-    {/*<ReactFlow*/}
-    {/*  nodes={[*/}
-    {/*    { id: '1', position: { x: 0, y: 0 }, data: { label: label } },*/}
-    {/*    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },*/}
-    {/*  ]}*/}
-    {/*  edges={[{ id: 'e1-2', source: '1', target: '2' }]}*/}
-    {/*/>*/}
-    </div>
-  )
-}
 export const CustomMarkdown = (props: Props) => {
   return (
     <div className={`${styles['md-viewer']} ${props.className}`} >
@@ -43,7 +22,7 @@ export const CustomMarkdown = (props: Props) => {
             h2: { component: Subtitle },
             p: { component: Paragraph },
             li: { component: Li },
-            // ReactFlow: { component: Test },
+            ReactFlow: { component: Test },
             // img: { component: Image },
           }
         }}
---
+++ app/ui/reactFlow/index.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import {ReactFlow} from "reactflow";
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const Test = () => {
+  return (
+    <div style={{width: '100%', height: `200px`}}>
+      <ReactFlow
+        nodes={initialNodes}
+        edges={initialEdges}
+      />
+    </div>
+  )
+}
+
+export {Test}
