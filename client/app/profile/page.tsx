8c5ebffc31bc3bfb19bc26d7359f22067f3dc7ed
push-ebx <nikita.keyn@yandex.ru>
2024-05-11 13:44:43 +0300

    страница со списком написанных статей

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -3,11 +3,14 @@
 import {getArticles} from "./api";
 import styles from './profile.module.scss'
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Article} from "@/app/lib/types";
 import {useAuth} from "@/app/lib/hooks/useAuth";
 import {Loader} from "@/app/ui/loader";
 import {useRouter} from "next/navigation";
+import {columns} from "@/app/ui/article-row";
+import {FloatButton, Table, Typography, Tooltip, Flex, Empty} from "antd";
+import {PlusOutlined} from "@ant-design/icons";
 
 export default function Dashboard() {
   const [articles, setArticles] = useState<Article[]>();
@@ -30,35 +33,21 @@ export default function Dashboard() {
         isFetchingUser && !user?.id ?
           <Loader/> :
           <div className={styles.list}>
-            {JSON.stringify(user)}
             {
-              articles &&
-              articles.map((article, i) =>
-                <div className={styles.item} key={article.id}>
-                  <span className={styles.number}>{i + 1}.</span>
-                  <Link className={styles.title} href={`/article?id=${article.id}`}>
-                    <h4>{article.title}</h4>
-                  </Link>
-                  <input
-                    type="checkbox"
-                    onClick={e => e.preventDefault()}
-                    className={styles.delete}
-                  />
-                  <input
-                    type="checkbox"
-                    onClick={e => e.preventDefault()}
-                    className={styles.edit}
-                  />
-                  <input
-                    type="checkbox"
-                    onClick={e => e.preventDefault()}
-                    className={styles.public}
-                  />
-                </div>
-              )
+              articles?.length ?
+                <Flex vertical>
+                  <Typography.Title level={2}>Список написанных статей</Typography.Title>
+                  <Table columns={columns} dataSource={articles} scroll={{ x: 100 }} />
+                </Flex> :
+                  <Empty className={styles.empty} description={"Вы еще не написал ни одной статьи!"} />
             }
           </div>
       }
+      <Link href={'/create'}>
+        <Tooltip title={"Создать статью"}>
+          <FloatButton icon={<PlusOutlined />} type="default" style={{ right: 50, bottom: 50 }} />
+        </Tooltip>
+      </Link>
     </div>
   );
 }
