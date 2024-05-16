9d5aba696c6a30b1f134f5688cb2f30e94b38033
push-ebx <nikita.keyn@yandex.ru>
2024-05-17 00:23:52 +0300

    адаптивы для статьи

--- client/app/ui/article-tree/index.tsx
+++ client/app/ui/article-tree/index.tsx
@@ -4,10 +4,13 @@ import React, {useEffect, useState} from 'react';
 import styles from "./article-tree.module.scss";
 import {Tree, TreeType} from "@/app/ui";
 import {getTree} from "@/app/article/api";
-import {Loader} from "@/app/ui/loader";
+import {FloatButton} from "antd";
+import {MenuOutlined} from "@ant-design/icons";
+import clsx from "clsx";
 
 export const ArticleTree = () => {
   const [tree, setTree] =  useState<TreeType>();
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
     getTree().then(res => {
@@ -16,9 +19,12 @@ export const ArticleTree = () => {
   }, []);
 
   return (
-    <div className={styles.wrapper_tree}>
-      <h2 style={{marginBottom: 20}}>Содержание</h2>
-      <Tree className={styles.tree} tree={tree}/>
-    </div>
+    <>
+      <div className={clsx(styles.wrapper_tree, isActive && styles.menu_active)}>
+        <h2 style={{marginBottom: 20}}>Содержание</h2>
+        <Tree className={styles.tree} tree={tree}/>
+      </div>
+      <FloatButton className={styles.isOpen} onClick={() => setIsActive(prev => !prev)} type={"primary"} icon={<MenuOutlined />}></FloatButton>
+    </>
   );
 };
